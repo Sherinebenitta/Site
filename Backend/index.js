5c5b33609dcb9e5224ce52840162b756974d7b80
sherinebenitta-it19 <sherinebenitta.it19@bitsathy.ac.in>
2025-05-15 11:06:28 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://sherine:flori@cluster0.i6fy0fl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+    dbName:"Booking"
+}).then(()=>{console.log("DATABASE CONNECTED SUCCESSFULLY")}).catch((error)=>{console.log(error)})
 
+app.use(express.json())
 app.listen(8000,()=>{
     console.log("SERVER STARTED")
 })
