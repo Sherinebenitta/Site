b1c18b2d4881df2ebab6956df819bb49a0809687
sherinebenitta-it19 <sherinebenitta.it19@bitsathy.ac.in>
2025-05-17 17:56:29 +0530

    img

--- Frontend/src/Entrypage/Credentials.jsx
+++ Frontend/src/Entrypage/Credentials.jsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
+import background from './download.jpeg'
 
 export default function Credentials(){
-    return<div>
+    return<div
+      className="container-fluid p-0"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+    >
         <Link to={'/User'}>User</Link><br/><br/>
         <Link to={'/Admin'}>Admin</Link>
     
