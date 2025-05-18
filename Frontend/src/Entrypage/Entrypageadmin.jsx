2ebf8148808c9074e1265f6d92b36a99ba9f5612
sherinebenitta-it19 <sherinebenitta.it19@bitsathy.ac.in>
2025-05-18 16:42:12 +0530

    entry

--- Frontend/src/Entrypage/Entrypageadmin.jsx
+++ Frontend/src/Entrypage/Entrypageadmin.jsx
@@ -3,14 +3,14 @@ import {Link,useNavigate} from 'react-router-dom'
 export default function Entryadmin(){
     const navigate = useNavigate()
     const logout = ()=>{
-        localStorage.clear();
+        localStorage.clear();      
         navigate('/')
 
     }
     return <div>
         <Link to={'/Train'}>Train</Link><br/><br/>
         <Link to={'/Routes'}>Route</Link><br/><br/>
-        <Link to={'/Seat'}>Seat</Link><br/><br/>
+        <Link to={'/Seat'}>Seat</Link><br/><br/>  
         <Link  to={'/Station'}>Station</Link><br/><br/>                                   
         <button onClick={logout}>Logout</button>
     </div>
