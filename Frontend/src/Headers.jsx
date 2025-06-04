1e797a96907fc315f55d1c6aaf4cb223de17a75f
sherinebenitta-it19 <sherinebenitta.it19@bitsathy.ac.in>
2025-06-04 10:34:33 +0530

    mm

--- Frontend/src/Headers.jsx
+++ Frontend/src/Headers.jsx
@@ -2,7 +2,7 @@
 export default function Header(){
   return<>
 <div className="bg-black text-center text-white pb-2">
-    <h1>TRAIN BOOKING SYSTEM</h1>
+    <h1>TRAIN BOOKING SITE</h1>
     </div>
   </>
 }
